3b20d476c2ec85ff3d6db00305da4fa67e755cf7
Denny Ferrassoli <denny.ferrassoli@gmail.com>
2018-05-02 12:04:23 -0700

    Add play api

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const crypto = require('crypto');
+
+const app = express()
+
+const game = {
+  players: []
+}
 
 app.set('port', (process.env.PORT || 5000))
 app.use('/public', express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
+app.get('/', (request, response) => {
   response.sendFile('index.html', {root: __dirname + '/public/'});
+})
+
+app.get('/play', (req, res) => {
+  if (!req.query.name) {
+    res.status(400).json({ error: 'Name is required' });
+  }
+  
+  if (req.query.name.length > 20) {
+    res.status(400).json({ error: 'Name should be 20 characters maximum' });
+  }
+  
+  
+  const id = crypto.randomBytes(16).toString('base64')
+  res.status(200).json({ id, req.query.name });
 })
 
 app.listen(app.get('port'), function() {
