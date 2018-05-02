66212ded67f7141a5539c5bd2a31b22f6054db8b
Denny Ferrassoli <denny.ferrassoli@gmail.com>
2018-05-02 10:25:03 -0700

    Better server

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
-const http = require('http')
-const port = 3000
-
-const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
-}
-
-const server = http.createServer(requestHandler)
-
-server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
-})
+var http = require('http'),
+    path = require('path'),
+    fs = require('fs');
+ 
+function getFile(filePath,res,page404){
+	fs.exists(filePath, (exists) => {
+		if (exists) {
+			fs.readFile(filePath, (err, contents) => {
+				if (!err) {
+					res.end(contents);
+				} else {
+					console.dir(err);
+				}
+			});
+		} else {
+			fs.readFile(page404, (err, contents) => {
+				if (!err) {
+					res.writeHead(404, {'Content-Type': 'text/html'});
+					res.end(contents);
+				} else {
+					console.dir(err);
+				};
+			});
+		};
+	});
+};
+ 
+function requestHandler(req, res) {
+	var	fileName = path.basename(req.url) || 'index.html',
+	    localFolder = __dirname + '/public/',
+	    page404 = localFolder + '404.html';
+ 
+	getFile((localFolder + fileName), res, page404);
+};
+ 
+http.createServer(requestHandler).listen(3000);
