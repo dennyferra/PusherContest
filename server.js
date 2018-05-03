40ab3690286b268b75a78f22a25169e1cf5819c7
Denny Ferrassoli <denny.ferrassoli@gmail.com>
2018-05-02 20:59:03 -0700

    Add options response

--- server.js
+++ server.js
@@ -14,10 +14,18 @@ app.use('/public', express.static(__dirname + '/public'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
   next();
 });
+
+app.options('/*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.send(200);
+})
 
 app.get('/', (request, response) => {
   response.sendFile('index.html', {root: __dirname + '/public/'});
