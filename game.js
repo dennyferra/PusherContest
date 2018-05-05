3e94a484a3f12223b0eff146f7c6e6dcdff3f4a7
Denny Ferrassoli <denny.ferrassoli@gmail.com>
2018-05-05 10:58:16 -0700

    Add game pusher connect

---
+++ game.js
@@ -0,0 +1,36 @@
+class Game {
+  constructor() {
+    this.pusher = new Pusher({
+      appId: process.env.APPID,
+      key: process.env.KEY,
+      secret: process.env.SECRET,
+      cluster: 'us2',
+      encrypted: true
+    });
+
+    this.presence = null;
+    this.users = [];
+  }
+
+  subscribe() {
+    this.presence = pusher.subscribe('presence-game');
+    this.presence.bind('pusher:subscription_error', data => {
+      console.log('presence.subscription_error', data);
+    });
+
+    this.presence.bind('pusher:subscription_succeeded', members => {
+      console.log('presence.subscription_succeeded', members);
+      //members.each(m => this.rootStore.game.members.push(m));
+    });
+
+    this.presence.bind('pusher:member_added', member => {
+      // todo
+    });
+
+    this.presence.bind('pusher:member_removed', member => {
+      // todo
+    });
+  }
+}
+
+export default Game;
--- server.js
+++ server.js
@@ -1,88 +1,86 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const crypto = require('crypto')
-const Pusher = require('pusher')
+const express = require('express');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const Pusher = require('pusher');
+const Game = require('./game');
 
-const app = express()
+const app = express();
 
-const pusher = new Pusher({
-  appId: process.env.APPID,
-  key: process.env.KEY,
-  secret: process.env.SECRET,
-  cluster: 'us2',
-  encrypted: true
-});
+const game = new Game();
+game.subscribe();
 
-const game = {
-  users: []
-}
-
-app.set('port', (process.env.PORT || 5000))
-app.use('/public', express.static(__dirname + '/public'))
+app.set('port', process.env.PORT || 5000);
+app.use('/public', express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X-UserId');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, Content-Length, X-Requested-With, X-UserId'
+  );
   next();
 });
 
 app.options('/*', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, X-UserId');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, Content-Length, X-Requested-With, X-UserId'
+  );
   res.sendStatus(200);
-})
+});
 
 app.get('/', (request, response) => {
-  response.sendFile('index.html', {root: __dirname + '/public/'});
-})
+  response.sendFile('index.html', { root: __dirname + '/public/' });
+});
 
 app.get('/play', (req, res) => {
   if (!req.query.nickname) {
-    res.status(400).json({ error: 'Name is required' })
-    return
+    res.status(400).json({ error: 'Name is required' });
+    return;
   }
 
-  const nickname = req.query.nickname.trim()
-  const nicknameLower = nickname.toLowerCase()
-  
+  const nickname = req.query.nickname.trim();
+  const nicknameLower = nickname.toLowerCase();
+
   if (nickname.length <= 2 || nickname.length > 20) {
-    res.status(400).json({ error: 'Name should be 3-20 characters in length' })
-    return
+    res.status(400).json({ error: 'Name should be 3-20 characters in length' });
+    return;
   }
 
   if (game.users.some(s => s.nicknameLower === nicknameLower)) {
-    res.status(400).json({ error: 'Nickname is not unique' })
-    return
+    res.status(400).json({ error: 'Nickname is not unique' });
+    return;
   }
-  
-  const id = crypto.randomBytes(16).toString('base64')
-  const user = { id, nickname, nicknameLower }
-  game.users.push(user)
-  res.status(200).json(user)
-})
+
+  const id = crypto.randomBytes(16).toString('base64');
+  const user = { id, nickname, nicknameLower };
+  game.users.push(user);
+  res.status(200).json(user);
+});
 
 app.post('/pusher/auth', (req, res) => {
   var socketId = req.body.socket_id;
   var channel = req.body.channel_name;
   var userId = req.headers['x-userid'];
 
-  console.log('Pusher Auth', socketId, channel, userId)
+  console.log('Pusher Auth', socketId, channel, userId);
 
   if (!userId) {
-    res.sendStatus(403)
-    return
+    res.sendStatus(403);
+    return;
   }
 
   const user = game.users.filter(user => user.id === userId);
 
-  console.log('User?', user)
+  console.log('User?', user);
 
   if (user.length <= 0) {
-    res.sendStatus(403)
-    return
+    res.sendStatus(403);
+    return;
   }
 
   var presenceData = {
@@ -90,15 +88,15 @@ app.post('/pusher/auth', (req, res) => {
     user_info: {
       name: user[0].nickname
     }
-  }
-  var auth = pusher.authenticate(socketId, channel, presenceData)
-  res.send(auth)
+  };
+  var auth = pusher.authenticate(socketId, channel, presenceData);
+  res.send(auth);
 });
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log('Node app is running at localhost:' + app.get('port'));
 
   pusher.trigger('game', 'user.join', {
-    "message": "hello world"
+    message: 'hello world'
   });
-})
+});
